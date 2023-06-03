8a32c8dd1d2c1e21b3e61b371bab5d9540bdd8ab
Ivan Mutsko <imucko1996@gmail.com>
2023-06-03 08:34:36 +0300

    add formater & validation

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,22 +1,48 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addContact } from 'redux/contacts/operations';
+import { selectAllContacts } from 'redux/contacts/selectors';
 import css from './ContactForm.module.css';
+import { formatPhoneNumber } from 'utils/formatPhoneNumber';
 
 const ContactForm = () => {
+  const contacts = useSelector(selectAllContacts);
   const dispatch = useDispatch();
 
   const handleSubmit = e => {
     e.preventDefault();
     const form = e.currentTarget;
-    const name = form.elements.name.value;
+    const name = form.elements.name.value.trim();
     const number = form.elements.number.value;
-    if (name !== '' || number !== '') {
-      const newContact = { name, number };
-      dispatch(addContact(newContact));
-      form.reset();
+
+    const isNameEmpty = name === '';
+    const isNumberValid = /^\d+$/.test(number) && number.length === 10;
+
+    if (isNameEmpty) {
+      alert('You have an empty field for name');
       return;
     }
-    alert('You have empty fields!');
+
+    if (!isNumberValid) {
+      alert('Invalid phone number. Please enter a 10-digit number');
+      return;
+    }
+
+    const isNameAlreadyExist = contacts.some(
+      contact => contact.name.toLowerCase().trim() === name.toLowerCase()
+    );
+
+    if (isNameAlreadyExist) {
+      alert(`${name} is already in contacts.`);
+      return;
+    }
+
+    const newContact = {
+      name,
+      number: formatPhoneNumber(number),
+    };
+
+    dispatch(addContact(newContact));
+    form.reset();
   };
 
   return (
---
+++ src/utils/formatPhoneNumber.js
@@ -0,0 +1,10 @@
+export const formatPhoneNumber = number => {
+  const cleaned = ('' + number).replace(/\D/g, '');
+  const match = cleaned.match(/^(\d{3})(\d{3})(\d{2})(\d{2})$/);
+
+  if (match) {
+    return '(' + match[1] + ')-' + match[2] + '-' + match[3] + '-' + match[4];
+  }
+
+  return null;
+};
